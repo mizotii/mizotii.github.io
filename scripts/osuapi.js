733a2f906f18c4499b309b2107fbd5750d9a6c4b
mizotii <mztiiiii0@gmail.com>
2024-01-15 23:03:56 -0800

    implement oauth

--- scripts/osuapi.js
+++ scripts/osuapi.js
@@ -1,1 +1,22 @@
-const url = new URL
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('auth-test').addEventListener('click', function() {
+        const url = new URL(
+            "https://osu.ppy.sh/oauth/authorize"
+        );
+    
+        const params = {
+            "client_id": "29626",
+            "redirect_uri": "https://sunchul.moe",
+            "response_type": "code",
+            "scope": "public identify",
+            "state": "randomval",
+        };
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    
+        fetch(url, {
+            method: "GET",
+        }).then(response => response.json());
+
+        window.open(url);
+    })
+})
