e6128e927d1dd3afeccb1daf7ca25c4ff57cc852
mizotii <mztiiiii0@gmail.com>
2024-05-14 09:56:18 -0700

    implement router

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,9 @@
 <script>
+    import { Router, Link, Route } from 'svelte-routing';
+    import Home from './lib/Home.svelte';
+    import Projects from './lib/Projects.svelte';
+
+    export let url = '';
 </script>
 
 
