0a231f5a701ea76c6e5475236b897e82d572099d
mizotii <omphen@uw.edu>
2024-06-04 16:31:51 -0700

    add content

--- src/lib/pages/Development.svelte
+++ src/lib/pages/Development.svelte
@@ -3,9 +3,17 @@
 </script>
 
 <Page>
-    <h1>DEVELOPMENT</h1>
-    <p>working on it!</p>
+    <h1>PROJECTS</h1>
+    <p>osu-heatmap: a calendar heatmap for the rhythm game osu! and all of its modes</p>
+    <div class='osu-heatmap'>
+        <a href='https://osu-heatm.app'>
+            <img src='https://i.imgur.com/vdWu1K5.png' alt='osu=heatmap'>
+        </a>
+    </div>
 </Page>
 
 <style>
+    .osu-heatmap > a > img {
+        width: 400px;
+    }
 </style>
--- src/lib/pages/Japanese.svelte
+++ src/lib/pages/Japanese.svelte
@@ -4,8 +4,26 @@
 
 <Page>
     <h1>JAPANESE</h1>
-    <p>working on it!</p>
+    <div class='certification'>
+        <img src='https://omph.s-ul.eu/tAFnz7ig' alt='my N1 certificate'>
+        <p>
+            In December 2023, I passed the Japanese Language Proficiency Test (JLPT) N1 after less than three years of studying completely on my own.
+        </p>
+        <p>
+            You can download my J-E translation of a short story from the Oregairu anthologies here:
+            <a href='/manuscript.pdf' download='manuscript'>斯くして、彼の前に新たな敵は現れる。</a>
+        </p>
+    </div>
 </Page>
 
 <style>
+    .certification {
+        margin: 24px;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .certification > img {
+        height: 400px;
+    }
 </style>
