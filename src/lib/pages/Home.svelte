47548e590731ba6c82afe5170f7fecea4fc82812
mizotii <omphen@uw.edu>
2024-07-19 15:17:31 -0700

    add to home

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -4,7 +4,7 @@
 
 <Page>
     <h1>WELCOME</h1>
-    <p>Hi! I'm Joel, a recent graduate of the University of Washington with a B.A. in English. Right now, I'm studying Computer Science at North Seattle College. Thanks for stopping by!</p>
+    <p>Hi! I'm Joel, a recent graduate of the University of Washington with a B.A. in English. Right now, I'm studying Computer Science at North Seattle College. As of now, this site really is just the bare minimum, I know, but thanks for stopping by anyway!</p>
 </Page>
 
 <style>
