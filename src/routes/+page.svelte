fcc7cecd0643f34b2af35d4ac5aa541a127c9963
mizotii <omphen@uw.edu>
2024-12-18 19:02:42 -0800

    add base page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<html data-theme="light" lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+</html>
+<div class="p-4 flex flex-col justify-center items-center">
+    <div class="avatar p-4">
+        <div class="w-48 md:w-64 rounded-full">
+            <img src="https://i.imgur.com/OXYQwQo.jpeg" />
+        </div>
+    </div>
+    <div class="p-2">
+        <p class="text-2xl text-center font-extralight">Joel Yang</p>
+    </div>
+    <div class="">
+        <p class="text-lg text-center font-extralight">developer, diehard Mariners fan, rhythm game enjoyer</p>
+    </div>
+    <div class="divider divider-neutral"></div>
+</div>
+<div class="p-4 flex flex-row">
+    <ul class="menu menu-sm md:menu-md lg:menu-lg bg-base-200 rounded-box w-96">
+        <li><a class="whitespace-nowrap">home ></a></li>
+        <li>
+            <h2 class="menu-title">projects</h2>
+            <ul>
+                <li><a class="whitespace-nowrap">dev ></a></li>
+                <li><a class="whitespace-nowrap">engl ></a></li>
+                <li><a class="whitespace-nowrap">jpn to engl ></a></li>
+            </ul>
+        </li>
+        <li>
+            <h2 class="menu-title">personal</h2>
+            <ul>
+                <li><a class="whitespace-nowrap">resume ></a></li>
+                <li><a class="whitespace-nowrap">blog ></a></li>
+                <li><a class="whitespace-nowrap">about ></a></li>
+                <li><a class="whitespace-nowrap">contact ></a></li>
+            </ul>
+        </li>
+    </ul>
+    <div class="divider divider-horizontal divider-neutral"></div>
+    <div>
+        <p class="text-2xl">[page]</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper ante ac orci ullamcorper, eget condimentum nunc cursus. Donec tempor, libero sed porttitor lacinia, nibh nibh vestibulum dolor, nec posuere justo justo nec nunc. Ut auctor imperdiet sem, in hendrerit metus. Phasellus aliquam tortor nec mattis lobortis. Nunc tempor gravida nulla. Vivamus ligula lectus, vehicula sit amet turpis ut, auctor pretium augue. Ut facilisis egestas fermentum. Vestibulum et posuere ex. Maecenas sodales dictum tellus. Maecenas iaculis efficitur lectus. In a diam massa. Integer vitae tincidunt purus, ac lacinia orci. Mauris aliquet scelerisque tellus, nec dictum ligula eleifend vel. Nunc cursus porta commodo.</p>
+    </div>
+    <div class="divider divider-neutral"></div>
+</div>
